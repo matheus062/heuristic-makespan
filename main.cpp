de871ec871c5736cee891888c7e82a43e3a1c480
matheus062 <matheussilvestre3@gmail.com>
2023-02-08 21:43:23 -0300

    Cria constantes dos tamanhos

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+float const r_15 = 1.5;
+float const r_20 = 2.0;
+
+int const m_size_10 = 10;
+int const m_size_20 = 20;
+int const m_size_50 = 50;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int m[10][]
+
+//  n = 'qtd tarefas alocadas';
+//  m = 'maquinas alocadas'
+
     return 0;
 }
+
+/*    M1   M2  M3
+ *  | 29 |   |   |
+ *  | 17 |   |   |
+ *  | 14 |   |   |
+ *  | 10 |   |   |
+ */
